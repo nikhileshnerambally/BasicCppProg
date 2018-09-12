dff9bf0f1bc7b53b7f44cba96729de64bba74cf3
nikhileshnerambally <nikhileshnerambally@gmail.com>
2018-09-12 18:35:19 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int Math = 90, Phy = 77, Chem = 69;
+    cout << setw(12) << "Maths = " << Math << endl << setw(12) << "Physics = " << Phy << endl << "Chemistry = " << Chem << endl;
+    return 0;
+}
